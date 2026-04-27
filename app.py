b88fee3857dff9b516a6865e8e367fb60989086b
Kai Voigt <k@123.org>
2026-04-27 15:31:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Welcome to Cloudera Education!"
+    return "Welcome to Cloudera Education 123!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
